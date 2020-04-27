6ac5ccc97204f1bc56f6d8fe644a9d1d2d8df91e
jabelli <jabelli@kavehome.com>
2020-04-27 12:25:59 +0200

    - Borramos el archivo postcss

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-var postcss = require('gulp-postcss');
-var gulp = require('gulp');
-
-gulp.task('css', function () {
-    return gulp.src('./src/*.css')
-        .pipe(postcss())
-        .pipe(gulp.dest('./dest'));
-});
